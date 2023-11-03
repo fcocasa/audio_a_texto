8b773a0adc932b0811c505329d039d01f22dd087
fcocasa <44985620+fcocasa@users.noreply.github.com>
2023-11-03 15:23:47 -0300

    Add files via upload

--- speech.py
+++ speech.py
@@ -1,26 +1,32 @@
 import speech_recognition as sr
 from pydub import AudioSegment
+from deepmultilingualpunctuation import PunctuationModel
 import json
+import re
 
 import json
 with open('data.json','r') as file_json:
     data = json.load(file_json)
     formato_entrada = data["formato_entrada"]
-    formato_salida = data["formato_salida"]
     audio_entrada = data["audio_entrada"]
-    audio_salida = data["audio_salida"]
     text_de_salida = data["text_de_salida"]    
 
-if formato_entrada != formato_salida:
+if formato_entrada != 'wav':
     sound = AudioSegment.from_file(audio_entrada, format=formato_entrada)
-    file_handle = sound.export(audio_salida, format=formato_salida)
-    filename = audio_salida
+    filename = f'{audio_entrada[:-4]}.wav'
+    file_handle = sound.export(filename, format='wav')
 else:
     filename = audio_entrada
+
 r = sr.Recognizer()
-# open the file
+
 with sr.AudioFile(filename) as source, open(text_de_salida,'w+') as file:
-    # listen for the data (load audio to memory)
     audio = r.listen(source)
     text = r.recognize_google(audio,language="es-ES")
     file.write(text)
+
+model = PunctuationModel(model = "kredor/punctuate-all")
+result = model.restore_punctuation(text)
+
+with open(f'puntuado_{text_de_salida}','w+') as out:
+    out.write(re.sub('\.','.\n',result))
